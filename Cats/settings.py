60aac520c98a9cf434ae1b7f66d71dedb867a0ea
koreanbloody <koreanbloody@gmail.com>
2022-07-21 23:53:50 +0900

    no message

--- Cats/settings.py
+++ Cats/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1tt3zrvb)7j-*o*6e^**ol(=k4lbjj8pv_=hki&75r_b(ep-m!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,6 +71,13 @@ WSGI_APPLICATION = 'Cats.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 
 
 
